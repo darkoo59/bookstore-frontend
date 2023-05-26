63877473e67d27cf875e6191bc38bf3bcc386473
Damjan <dimitrijevicdamjandd@gmail.com>
2023-05-26 23:16:45 +0200

    Removed unused imports from Books

--- bookstore/src/pages/Books.tsx
+++ bookstore/src/pages/Books.tsx
@@ -1,12 +1,6 @@
 
 import * as React from "react";
 import BannerBackground from "../images/banner-background.png";
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import { API_BASE_URL } from '../config';
 import { Book } from '../model/book';
 import { BookCard } from '../components/book-card';
