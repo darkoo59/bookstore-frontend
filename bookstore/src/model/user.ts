1686e1274ec3cfe56655338c5dd927ae794be015
Rade Stojanovic <stojanovicrade@gmail.com>
2023-05-27 17:51:57 +0200

    model updated

--- bookstore/src/model/user.ts
+++ bookstore/src/model/user.ts
@@ -1,14 +1,14 @@
 import { Address } from "./address";
 
 export interface User {
-    email?: string;
-    password?: string;
-    firstname?: string;
-    lastname?: string;
-    address?: Address;
-    gender?: number;
-    phone?: string;
-    nationalId?: string;
-    occupation?: string;
-    information?: string;
-  }
+  email?: string;
+  password?: string;
+  firstname?: string;
+  lastname?: string;
+  address?: Address;
+  gender?: number;
+  phone?: string;
+  nationalId?: string;
+  occupation?: string;
+  information?: string;
+}
--- bookstore/src/pages/Books.tsx
+++ bookstore/src/pages/Books.tsx
@@ -1,9 +1,8 @@
-
 import * as React from "react";
 import BannerBackground from "../images/banner-background.png";
-import { API_BASE_URL } from '../config';
-import { Book } from '../model/book';
-import { BookCard } from '../components/book-card';
+import { API_BASE_URL } from "../config";
+import { Book } from "../model/book";
+import { BookCard } from "../components/book-card";
 import Navbar from "../components/navbar";
 import { useIsAuthenticated } from "react-auth-kit";
 import { toast } from "react-toastify";
@@ -11,106 +10,129 @@ import { BookWithCharacteristics } from "../model/bookWithCharacteristics";
 import BookWithCharacteristicsCard from "../components/bookWithCharacteristics-card";
 import { RatingDTO } from "../dto/ratingDTO";
 import { Container, Grid } from "@mui/material";
-    
-
+import { useCallback } from "react";
 
 const Books = () => {
-    const [books, setBooks] = React.useState<Book[]>([]);
-    const [booksWithCharacteristics, setBookWithCharacteristics] = React.useState<BookWithCharacteristics[]>([]);
-    const isAuthenticated = useIsAuthenticated();
-    const [userRatings, setUserRatings] = React.useState<RatingDTO[]>([]);
-    const [loading, setLoading] = React.useState(true);
+  const [books, setBooks] = React.useState<Book[]>([]);
+  const [booksWithCharacteristics, setBookWithCharacteristics] = React.useState<
+    BookWithCharacteristics[]
+  >([]);
+  const isAuthenticated = useIsAuthenticated();
+  const [userRatings, setUserRatings] = React.useState<RatingDTO[]>([]);
+  const [loading, setLoading] = React.useState(true);
 
-    
-    React.useEffect(() => {
-      if (!isAuthenticated()) {
-        fetchBooksWithCharacteristics();
-      } else {
-        fetchBooks();
+  const fetchBooks = useCallback(async () => {
+    const response = await fetch(API_BASE_URL + "/book");
+    const data = await response.json();
+    setBooks(data);
+    if (!isAuthenticated()) setLoading(false);
+  }, [isAuthenticated])
+
+  const fetchBooksWithCharacteristics = useCallback(async () => {
+    try {
+      const response = await fetch(API_BASE_URL + "/book/characteristics");
+      if (!response.ok) {
+        toast.error("Error fetching books", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+        return;
       }
-    }, []);
+      const data: BookWithCharacteristics[] = await response.json();
+      setBookWithCharacteristics(data);
+    } catch (error: any) {
+      console.log(error);
+      toast.error("Error fetching books", {
+        position: toast.POSITION.TOP_CENTER,
+      });
+    }
+  }, []);
 
-    React.useEffect(() => {
-      if(isAuthenticated())
-        fetchRatings();
-    }, []);
+  React.useEffect(() => {
+    if (!isAuthenticated()) {
+      fetchBooksWithCharacteristics();
+    } else {
+      fetchBooks();
+    }
+  }, [fetchBooks, isAuthenticated, fetchBooksWithCharacteristics]);
+
+  React.useEffect(() => {
+    if (isAuthenticated()) fetchRatings();
+  }, [isAuthenticated]);
+
   
-    const fetchBooks = async () => {
-      const response = await fetch(API_BASE_URL+'/book');
-      const data = await response.json();
-      setBooks(data);
-      if(!isAuthenticated())
-      setLoading(false)
+
+  const fetchRatings = async () => {
+    const token =
+      document.cookie
+        .match("(^|;)\\s*" + "accessToken" + "\\s*=\\s*([^;]+)")
+        ?.pop() || "";
+    const response = await fetch(API_BASE_URL + "/rating/user-ratings", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUserRatings(data);
+    setLoading(false);
+  };
+
+  const getUserRankingForBook = (bookId: number) => {
+    const rating = userRatings.find((rating) => rating.bookId === bookId);
+    if (rating) {
+      return rating.rating;
     }
+    return -1;
+  };
 
-    const fetchBooksWithCharacteristics = async () => {
-      try {
-        const response = await fetch(API_BASE_URL+'/book/characteristics');
-        if (!response.ok) {
-          toast.error("Error fetching books", { position: toast.POSITION.TOP_CENTER });
-          return;
-        }
-        const data : BookWithCharacteristics[] = await response.json();
-        setBookWithCharacteristics(data);
-      } catch (error: any) {
-        console.log(error);
-        toast.error("Error fetching books", { position: toast.POSITION.TOP_CENTER });
-      }
-    }
-
-    const fetchRatings = async () => {
-      const token = document.cookie.match('(^|;)\\s*' + "accessToken" + '\\s*=\\s*([^;]+)')?.pop() || '';
-      const response = await fetch(API_BASE_URL+'/rating/user-ratings', {headers : {'Authorization': `Bearer ${token}`}});
-      const data = await response.json();
-      setUserRatings(data);
-      setLoading(false);
-    }
-
-    const getUserRankingForBook = (bookId: number) => {
-      const rating = userRatings.find((rating) => rating.bookId === bookId);
-      if (rating) {
-        return rating.rating;
-      }
-      return -1;
-    };
-
-
+  console.log(books);
   return (
     <div>
-    <div className="home-container">
-    <Navbar/>
-    <div className="home-banner-containter">
-            <div className="home-bannerImage-container">
+      <div className="home-container">
+        <Navbar />
+        <div className="home-banner-containter">
+          <div className="home-bannerImage-container">
             <img src={BannerBackground} alt="" />
-            </div>
-    </div>
-    </div>
-    <div>
-    </div>
-      { isAuthenticated() && 
-      <div className="card-container">
-        { isAuthenticated() && books.map((book) => (
-            <React.Fragment key={book.id}>
-            <BookCard book={book} userRating={getUserRankingForBook(book.id)}/>
-            </React.Fragment>
-        ))}
-      </div> }
-      { !isAuthenticated() && 
-      <Container>
-        <Grid container spacing={2} sx={{ mt: 3}}>
-          <Grid item lg={6}>
-            { booksWithCharacteristics
-                .filter(bc => !bc.suggested)
-                .map(bc => (<BookWithCharacteristicsCard bookWithCharacteristics={bc} key={bc.book.id}/>)) }
+          </div>
+        </div>
+      </div>
+      <div></div>
+      {isAuthenticated() && (
+        <div className="card-container">
+          {isAuthenticated() &&
+            books.map((book) => (
+              <React.Fragment key={book.id}>
+                <BookCard
+                  book={book}
+                  userRating={getUserRankingForBook(book.id)}
+                />
+              </React.Fragment>
+            ))}
+        </div>
+      )}
+      {!isAuthenticated() && (
+        <Container>
+          <Grid container spacing={2} sx={{ mt: 3 }}>
+            <Grid item lg={6}>
+              {booksWithCharacteristics
+                .filter((bc) => !bc.suggested)
+                .map((bc) => (
+                  <BookWithCharacteristicsCard
+                    bookWithCharacteristics={bc}
+                    key={bc.book.id}
+                  />
+                ))}
             </Grid>
-          <Grid item lg={6}>
-              { booksWithCharacteristics
-                .filter(bc => bc.suggested)
-                .map(bc => (<BookWithCharacteristicsCard bookWithCharacteristics={bc} key={bc.book.id}/>)) }
+            <Grid item lg={6}>
+              {booksWithCharacteristics
+                .filter((bc) => bc.suggested)
+                .map((bc) => (
+                  <BookWithCharacteristicsCard
+                    bookWithCharacteristics={bc}
+                    key={bc.book.id}
+                  />
+                ))}
+            </Grid>
           </Grid>
-        </Grid>
-      </Container> 
-      }
+        </Container>
+      )}
     </div>
   );
 };
